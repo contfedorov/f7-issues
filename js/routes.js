25d15ec3bf6cbe8c6f4e5cd76fb8601a38982923
Egor A. Fedorov <efedorov@dataeast.com>
2020-07-28 11:57:06 +0700

    bugfixing: update Framework7 to v5.7.6

--- js/routes.js
+++ js/routes.js
@@ -2,6 +2,12 @@ routes = [
   {
     path: '/',
     url: './index.html',
+      on: {
+          pageInit: function (e, page) {
+              console.log("index page inited");
+              // app.tab.show('#tab3', false)
+          }
+      }
   },
   // Default route (404 page). MUST BE THE LAST
   {
