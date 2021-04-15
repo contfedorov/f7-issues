3b9cb683711ddf73c4714a10510437ac99480d82
Egor A. Fedorov <efedorov@dataeast.com>
2021-04-15 19:36:03 +0700

    code formatted

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,11 +1,11 @@
 routes = [
-  {
-    path: '/',
-    url: './index.html',
-  },
-  // Default route (404 page). MUST BE THE LAST
-  {
-    path: '(.*)',
-    url: './pages/404.html',
-  },
+    {
+        path: '/',
+        url: './index.html',
+    },
+    // Default route (404 page). MUST BE THE LAST
+    {
+        path: '(.*)',
+        url: './pages/404.html',
+    },
 ];
